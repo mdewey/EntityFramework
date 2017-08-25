356c3154a31e5dd08db2b4c6ec7e7a634469fe0b
Mark Dewey <mark.t.dewey@gmail.com>
2017-08-25 12:13:05 -0400

    added seed method for blogs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,6 +24,52 @@ namespace EntityFramework.Controllers
             return View(_context.Blogs.ToList());
         }
 
+
+        public IActionResult Seed()
+        {
+            var seedBlags = new List<Blog>(){
+                new Blog {
+                        Author = "me",
+                        Body = "lorem ipsum body",
+                        Description = "Cool article 1",
+                        IsPublished = true,
+                        Url = "http://www.google.com"
+                    },
+                    new Blog {
+                        Author = "me",
+                        Body = "lorem ipsum body",
+                        Description = "Cool article 2",
+                        IsPublished = true,
+                        Url = "http://www.google.com"
+                    },
+                    new Blog {
+                        Author = "me",
+                        Body = "lorem ipsum body",
+                        Description = "Cool article 3",
+                        Url = "http://www.google.com"
+                    },
+                    new Blog {
+                        Author = "me",
+                        Body = "lorem ipsum body",
+                        Description = "Cool article 4",
+                        Url = "http://www.google.com"
+                    },new Blog {
+                        Author = "me",
+                        Body = "lorem ipsum body",
+                        Description = "Cool article 5",
+                        IsPublished = true,
+                        Url = "http://www.google.com"
+                    },
+            };
+
+            seedBlags.ForEach(blag => _context.Blogs.Add(blag));
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
